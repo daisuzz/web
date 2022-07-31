8f175ba8440670019d637f8e9c43194630e16a74
daisuzz <daisaku.rugby@gmail.com>
2022-07-31 22:46:43 +0900

    add image

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 // @ts-ignore
-import {container} from "./Layout.module.css"
 import Header from "./organisms/Header";
 import {CssBaseline, Paper, ThemeProvider} from "@mui/material";
 import {theme} from "../assets/theme";
@@ -10,7 +9,6 @@ interface Props {
     children: React.ReactNode
 }
 
-
 const Layout: React.FC<Props> = ({pageTitle, children}) => {
     return (
         <ThemeProvider theme={theme}>
@@ -18,7 +16,7 @@ const Layout: React.FC<Props> = ({pageTitle, children}) => {
             <title>{pageTitle} | daisuzz.log</title>
             <Header/>
             <main>
-                <Paper elevation={0} sx={{background: '#FFFFFF', margin: 5}}>
+                <Paper elevation={0} sx={{margin: 5}}>
                     {children}
                 </Paper>
             </main>

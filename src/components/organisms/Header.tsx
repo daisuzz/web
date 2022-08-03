f86869b257d63906cb2b8c3494f026d0c30a7645
daisuzz <daisaku.rugby@gmail.com>
2022-08-03 21:51:48 +0900

    responsive header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import {AppBar, Box, Container, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Container, IconButton, Toolbar, Typography} from "@mui/material";
 import MenuButton from "../atoms/MenuButton";
 import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
 import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+import {Link} from "gatsby";
 
 const ResponsiveAppBar = () => {
     return (
@@ -25,11 +26,32 @@ const ResponsiveAppBar = () => {
                     >
                         daisuzz.dev
                     </Typography>
-                    <div style={{flexGrow: 1}}></div>
-                    <Box sx={{display: {md: 'flex'}}}>
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}/>
+                    <Box sx={{display: {xs: 'none', md: 'flex'}}}>
                         <MenuButton label="TOP" href="/" icon={<HomeIcon/>}/>
                         <MenuButton label="ABOUT" href="/about" icon={<PersonIcon/>}/>
                         <MenuButton label="BLOG" href="/blogs" icon={<TextSnippetIcon/>}/>
+                    </Box>
+                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}/>
+                    <Box sx={{display: {xs: 'flex', md: 'none'}}}>
+                        <IconButton
+                            component={Link}
+                            to={"/"}
+                            sx={{color: 'inherit', textDecoration: 'none'}}>
+                            <HomeIcon/>
+                        </IconButton>
+                        <IconButton
+                            component={Link}
+                            to={"/about"}
+                            sx={{color: 'inherit', textDecoration: 'none'}}>
+                            <PersonIcon/>
+                        </IconButton>
+                        <IconButton
+                            component={Link}
+                            to={"/blogs"}
+                            sx={{color: 'inherit', textDecoration: 'none'}}>
+                            <TextSnippetIcon/>
+                        </IconButton>
                     </Box>
                 </Toolbar>
             </Container>

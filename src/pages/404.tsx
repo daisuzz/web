1dc16c7f1bce95d58e1feb097d97e8c07ab1e727
daisuzz <daisaku.rugby@gmail.com>
2024-02-29 14:22:17 +0900

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,8 +5,8 @@ import Layout from "../components/Layout"
 const NotFoundPage: React.FC = () => {
     return (
         <Layout pageTitle="404">
-            <h1>404: Not Found</h1>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <h1>404 Not Found</h1>
+            <p>The page can’t be found.</p>
         </Layout>
     )
 }

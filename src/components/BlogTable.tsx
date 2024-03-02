d45d0545001ae6b3fe757e3a0e38edf7d0f4847e
daisuzz <daisaku.rugby@gmail.com>
2024-03-02 15:53:02 +0900

    polish

--- src/components/BlogTable.tsx
+++ src/components/BlogTable.tsx
@@ -18,7 +18,7 @@ export interface ExternalBlog {
 
 const BlogTable: React.FC<BlogArticleTableProps> = ({qiitaBlogs, hatenaBlogs}) => {
     const [value, setValue] = React.useState('0');
-    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    const handleChange = (_: React.SyntheticEvent, newValue: string) => {
         setValue(newValue);
     };
     return (
@@ -30,96 +30,62 @@ const BlogTable: React.FC<BlogArticleTableProps> = ({qiitaBlogs, hatenaBlogs}) =
                 </TabList>
             </Box>
             <TabPanel value='0' sx={{padding: 0}}>
-                <TableContainer component={Paper} elevation={0}>
-                    <Table size="small">
-                        <TableBody>
-                            {hatenaBlogs.map(blog => {
-                                const title = blog.title || blog.id
-                                const publishedDate = DateUtils.formatDate(
-                                    new Date(Date.parse(blog.publishedAt)),
-                                    "YYYY年MM月DD日 hh:mm"
-                                )
-                                return (
-                                    <TableRow key={blog.id}
-                                              sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                                    >
-                                        <TableCell align="left">
-                                            <Typography variant="body1" sx={{
-                                                margin: "10px auto"
-                                            }}>
-                                                <Link
-                                                    component={"a"}
-                                                    href={blog.link}
-                                                    rel="noreferrer noopener"
-                                                    target="_blank"
-                                                    color="inherit"
-                                                    underline="none"
-                                                >
-                                                    {title}
-                                                </Link>
-                                            </Typography>
-                                            <Box sx={{display: {xs: 'flex', md: 'flex'}}}>
-                                                <EventAvailableIcon fontSize="small"/>
-                                                <Typography variant="body2" sx={{
-                                                    margin: "auto auto auto 5px"
-                                                }}>
-                                                    {publishedDate}
-                                                </Typography>
-                                            </Box>
-                                        </TableCell>
-                                    </TableRow>
-                                )
-                            })}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                {tableTemplate(hatenaBlogs)}
             </TabPanel>
             <TabPanel value='1' sx={{padding: 0}}>
-                <TableContainer component={Paper} elevation={0}>
-                    <Table size="small">
-                        <TableBody>
-                            {qiitaBlogs.map(blog => {
-                                const title = blog.title || blog.id
-                                const publishedDate = DateUtils.formatDate(
-                                    new Date(Date.parse(blog.publishedAt)),
-                                    "YYYY年MM月DD日 hh:mm"
-                                )
-                                return (
-                                    <TableRow key={blog.id}
-                                              sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                                    >
-                                        <TableCell align="left">
-                                            <Typography variant="body1" sx={{
-                                                margin: "10px auto"
-                                            }}>
-                                                <Link
-                                                    component={"a"}
-                                                    href={blog.link}
-                                                    rel="noreferrer noopener"
-                                                    target="_blank"
-                                                    color="inherit"
-                                                    underline="none"
-                                                >
-                                                    {title}
-                                                </Link>
-                                            </Typography>
-                                            <Box sx={{display: {xs: 'flex', md: 'flex'}}}>
-                                                <EventAvailableIcon fontSize="small"/>
-                                                <Typography variant="body2" sx={{
-                                                    margin: "auto auto auto 5px"
-                                                }}>
-                                                    {publishedDate}
-                                                </Typography>
-                                            </Box>
-                                        </TableCell>
-                                    </TableRow>
-                                )
-                            })}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                {tableTemplate(qiitaBlogs)}
             </TabPanel>
         </TabContext>
+    )
+}
+
+function tableTemplate(blogs: ExternalBlog[]): React.JSX.Element {
+    return (
+        <TableContainer component={Paper} elevation={0}>
+            <Table size="small">
+                <TableBody>
+                    {blogs.map(blog => tableRowTemplate(blog))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+function tableRowTemplate(blog: ExternalBlog): React.JSX.Element {
+    const title = blog.title || blog.id
+    const publishedDate = DateUtils.formatDate(
+        new Date(Date.parse(blog.publishedAt)),
+        "YYYY年MM月DD日 hh:mm"
+    )
+    return (
+        <TableRow key={blog.id}
+                  sx={{'&:last-child td, &:last-child th': {border: 0}}}
+        >
+            <TableCell align="left">
+                <Typography variant="body1" sx={{
+                    margin: "10px auto"
+                }}>
+                    <Link
+                        component={"a"}
+                        href={blog.link}
+                        rel="noreferrer noopener"
+                        target="_blank"
+                        color="inherit"
+                        underline="none"
+                    >
+                        {title}
+                    </Link>
+                </Typography>
+                <Box sx={{display: {xs: 'flex', md: 'flex'}}}>
+                    <EventAvailableIcon fontSize="small"/>
+                    <Typography variant="body2" sx={{
+                        margin: "auto auto auto 5px"
+                    }}>
+                        {publishedDate}
+                    </Typography>
+                </Box>
+            </TableCell>
+        </TableRow>
     )
 }
 
